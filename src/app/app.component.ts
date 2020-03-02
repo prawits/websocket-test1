15ac47896bc781697547ddabc9db970cbdf66816
identify-rfid <identify.rfid.co.th@gmail.com>
2020-03-02 18:01:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { webSocket } from 'rxjs/websocket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//const ws: WebSocketSubject<any> = webSocket("ws://203.154.88.148:5010");
+
 export class AppComponent {
   title = 'websocket-test1';
+  username = '';
+  listTag = [];
+  subject = webSocket({
+    url: 'ws://203.154.88.148:5010',
+    deserializer: msg => msg
+  });
+  
+
+  ngOnInit() {
+    //this.subject.asObservable().subscribe(dataFromServer => console.log( dataFromServer));
+    //this.ws.next({message: 'some message'});
+    // this.ws.subscribe(
+    //   msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+    //   err => console.log(err)
+    // );
+
+    this.subject.subscribe(
+      msg => console.log(this.listTag.push(msg.data)), // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+   
+  }
+
+  // @HostListener('window:beforeunload', [ '$event' ])
+  // beforeUnloadHander(event) {
+  //   subject.unsubscribe();
+  //   alert('call beforeunload');
+  // }
+
+  
+  //ss = new WebSocket();
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+//import { webSocket } from "rxjs/webSocket";
 @NgModule({
   declarations: [
     AppComponent
